0cb9907ec748de661cc721ec897e1d45841e3aa4
Toughrebel4041 <raihansultanpb@gmail.com>
2024-03-11 23:40:21 +0700

    Finishing Login Page

--- latihan3_login.php
+++ latihan3_login.php
@@ -1,16 +1,19 @@
 <?php
-    if (isset($_POST['submit'])) {
-        $name=$_POST['name'];
-        $email=$_POST['email'];
-        $pass=$_POST['pass'];
-        $bday=$_POST['bday'];
-        $date=$_POST['date'];
-        $study=$_POST['study'];
-        $address=$_POST['address'];
-        $phone=$_POST['phone'];
-        $gender=$_POST['gender'];
-        echo "welcome $name";
-    }
+if(isset($_POST['submit'])){
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $pass=$_POST['pass'];
+    $bday=$_POST['bday'];
+    $date=date_create($bday);
+    $study=$_POST['study'];
+    $address=$_POST['address'];
+    $phone=$_POST['phone'];
+    $gender=$_POST['gender'];
+
+    ?>
+    <h3> Welcome <?php $name; ?>
+    <?php
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,8 +21,51 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latihan 3 - Login - Raihan Sultan</title>
-</head>
-<body>
-    
+<style>
+button {   
+       width: 100%;  
+        padding: 15px;   
+        margin: 10px 0px;   
+        border: none;   
+        cursor: pointer;   
+         }   
+ form {   
+        border: 3px solid #f1f1f1;   
+    }   
+ input[type=text], input[type=password] {   
+        width: 100%;   
+        margin: 8px 0;  
+        padding: 12px 20px;   
+        display: inline-block;  
+        box-sizing: border-box;   
+    }  
+ button:hover {   
+        opacity: 0.7;   
+    }   
+  .cancelbtn {   
+        width: auto;   
+        padding: 10px 18px;  
+        margin: 10px 5px;  
+    }   
+ .container {   
+        padding: 25px;   
+        background-color: lightblue;  
+    }   
+</style>   
+</head>    
+<body>    
+    <h3> Welcome <?php $name; ?>
+    <center> <h1> Student Login Form </h1> </center>   
+    <form>  
+        <div class="container">   
+            <label>Username : </label>   
+            <input type="text" placeholder="Enter Username" name="username" required>  
+            <label>Password : </label>   
+            <input type="password" placeholder="Enter Password" name="password" required>  
+            <button type="submit">Login</button>   
+            <input type="checkbox" checked="checked"> Remember me   
+            <button type="button" class="cancelbtn"> Cancel</button>   
+        </div>   
+    </form>   
 </body>
 </html>
